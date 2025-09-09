cf9ca029c3266f5c30716730e44845c4d157cf63
fiaz ahmad <fiazahmadf1983@gmail.com>
2025-09-09 17:56:24 +0500

    update

---
+++ mern stack cources/9. introduction of javascript/index.js
@@ -0,0 +1,7 @@
+function getmilk(){
+    console.lpg("leave the house")
+    console.log("go to the store");
+    console.log("buy 2 litre of milk");
+    console.log("come back home");
+}
+getmilk();
